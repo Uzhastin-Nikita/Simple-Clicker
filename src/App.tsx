2a44cbaac95001b6ddd9dc6eec13a176f70af0ed
Uzhastin-Nikita <fosfor20@mail.ru>
2023-12-09 01:24:18 +0300

    feat(app): add type2Upgrade to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-// src/App.tsx
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   click,
   buyUpgrade,
   selectTotalMoney,
+  resetType2Upgrade,
 } from "./moneySlice";
 import {
   buyType1Upgrade,
-  selectType1Cost,
-  selectType1Level,
+  buyType2Upgrade,
+  selectType1UpgradeCost,
+  selectType1UpgradeLevel,
+  selectType2UpgradeActivated,
+  selectType2UpgradeCost,
+  selectType2UpgradeIncomePerSecond,
 } from "./upgradeSlice";
-import { RootState } from "./app/store";
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const totalMoney = useSelector(selectTotalMoney);
-  const type1Level = useSelector(selectType1Level);
-  const type1Cost = useSelector(selectType1Cost);
+  const type1Level = useSelector(selectType1UpgradeLevel) as number;
+  const type1Cost = useSelector(selectType1UpgradeCost);
+  const type2Activated = useSelector(selectType2UpgradeActivated);
+  const type2IncomePerSecond = useSelector(selectType2UpgradeIncomePerSecond);
+  const type2Cost = useSelector(selectType2UpgradeCost);
 
   const handleUpgradeClick = () => {
     dispatch(buyUpgrade({ cost: type1Cost }));
     dispatch(buyType1Upgrade());
   };
+
+  const handleType2UpgradeClick = () => {
+    dispatch(buyUpgrade({ cost: type2Cost }));
+    dispatch(buyType2Upgrade());
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (type2Activated) {
+        // @ts-ignore
+        dispatch(click(type2IncomePerSecond));
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [dispatch, type2Activated, type2IncomePerSecond]);
 
   return (
     <div>
@@ -32,6 +54,13 @@ const App: React.FC = () => {
         Buy Type 1 Upgrade (Cost: {type1Cost})
       </button>
       <p>Type 1 Upgrade Level: {type1Level}</p>
+      <button
+        onClick={handleType2UpgradeClick}
+        disabled={totalMoney < type2Cost}
+      >
+        Buy Type 2 Upgrade (Cost: {type2Cost})
+      </button>
+      {type2Activated && <p>Type 2 Upgrade Activated!</p>}
     </div>
   );
 };
