e116d08e74314396008b294140001307b39df0df
Uzhastin-Nikita <fosfor20@mail.ru>
2023-12-09 13:09:55 +0300

    feat(package): add file loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,39 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: "development",
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.(tsx?|ts)$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'), // указываем папку public как корень сервера
+      directory: path.join(__dirname, "public"),
     },
     port: 3000,
     hot: true,
