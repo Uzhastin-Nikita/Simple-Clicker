e3d7988874449ceed9bda5730731de850f59a913
Uzhastin-Nikita <fosfor20@mail.ru>
2024-01-12 23:46:09 +0300

    refactor: delete unnessesary imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
-import { createStore, combineReducers } from "redux";
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import moneyReducer from "./moneySlice";
 import upgradeReducer from "./upgradeSlice";
 
